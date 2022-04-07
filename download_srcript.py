cb74f5084bffdb8128fc24891ceeadbacf93a82e
jmarrero_ <58149356+juanmdom@users.noreply.github.com>
2022-04-07 17:19:39 +0100

    Add files via upload

---
+++ download_srcript.py
@@ -0,0 +1,54 @@
+# Script para ordenar la carpeta de Downloads
+
+from fileinput import filename
+import os, shutil
+
+# The folder you want to sort
+folder_path = r"C:\Users\juanmdom\Downloads"
+
+# File extensions that will be reorganized in specific folders
+extensions = [".docx", ".txt", ".doc", ".pdf", ".mp4", 
+              ".jpg", ".jpeg", ".gif", ".png", ".rar",
+              ".zip", ".exe", ".torrent"]
+
+# Stores all files of the folder
+files = [os.path.join(folder_path, file) 
+          for file in os.listdir(folder_path) 
+            if os.path.isfile(os.path.join(folder_path, file))]
+
+# Stores all folders of the folder
+folders = [os.path.join(folder_path, file) 
+            for file in os.listdir(folder_path) 
+                if not os.path.isfile(os.path.join(folder_path, file))]
+
+# Main function
+def main():
+  # Checking every file    
+  for file in files:
+    # Splits the file name and it's extension
+    filename, fextension = os.path.splitext(file)
+
+    # If the extension is in the array it creates a specific folder 
+    # for that format
+    if fextension in extensions:
+      # The new folder's name will be the extension of the file
+      new_folder_name = fextension.replace(".", "")
+      # Checks if the folder where we move the file do exists
+      if os.path.isdir((folder_path + "\\" + new_folder_name)):
+        shutil.move(file, folder_path + "\\" + new_folder_name)
+      # If not we make a new dir
+      else:
+        os.makedirs(folder_path + "\\" + new_folder_name)
+        shutil.move(file, folder_path + "\\" + new_folder_name)
+    
+    # If that format isn't in the array, we build 'others' folder
+    else:
+      new_folder_name = "others"
+      if os.path.isdir((folder_path + "\\" + new_folder_name)):
+        shutil.move(file, folder_path + "\\" + new_folder_name)
+      else:
+        os.makedirs(folder_path + "\\" + new_folder_name)
+        shutil.move(file, folder_path + "\\" + new_folder_name)
+
+if __name__ == "__main__":
+  main()
